53781635dc7d33835ba8ef657131fd56452785c5
Kudah Shambare <Kudahshambare263@gmail.com>
2021-04-02 22:48:41 +0200

    level 100 done

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+for(let i=0;i<episodeList.length;i++){
+  let episodeFrame=document.createElement("section"); //A container for a single episode
+episodeFrame.id="episode-frame";
+  let episodeName=document.createElement("h4"); // Episode title, season number and episode number
+  episodeFrame.appendChild(episodeName);
+  let episodeImage=document.createElement("img");// Episode Image
+  episodeImage.id="image";
+  episodeFrame.appendChild(episodeImage);
+  let episodeDescription=document.createElement("div") //Episode Description
+  episodeDescription.innerHTML=episodeList[i].summary;
+  episodeDescription.style.justifyItems="center";
+  episodeFrame.appendChild(episodeDescription);
+  episodeDescription
+   rootElem.appendChild(episodeFrame);
+   let seasonAndEpisodeNumber="S"+episodeList[i].season+"E"+episodeList[i].number;
+   if(episodeList[i].season<10){
+     seasonAndEpisodeNumber =
+       "S0" + episodeList[i].season + "E" + episodeList[i].number;
+   }
+    if (episodeList[i].number< 10) {
+      seasonAndEpisodeNumber =
+        "S0" + episodeList[i].season + "E0" + episodeList[i].number;
+    }
+  episodeName.innerHTML=episodeList[i].name+" "+seasonAndEpisodeNumber;
+   episodeImage.src=episodeList[i].image.medium;
+}
 }
 
 window.onload = setup;
