10acc0434f1f8d3a1d06dda31f1657f2f792f55d
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-01-24 03:20:58 +0200

    Level 100 Done

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 //Getting all episodes from episode.js file
-const allEpisodes=getAllEpisodes();
+const allEpisodes = getAllEpisodes();
 console.log(allEpisodes);
 
 //Get the episode container
-const episodesContainer=document.getElementById("episodesContainer");
- //A function to enable us to rerender the details with different info
- const createEpisodeTemplate=(episode)=>{
-     //Create a section to store each episode detail
-const episodeDetails=document.createElement("section");
-episodeDetails.id="episodeDetails"
-const episodeImage=document.createElement("img");
-const episodeInfo=document.createElement("section");
-//Episode Info
-const episodeName=document.createElement("h4");
-const seasonNumber=document.createElement("h4");
-const episodeNumber=document.createElement("h4");
-const episodeSummary=document.createElement("p");
+const episodesContainer = document.getElementById("episodesContainer");
+//Create a function to format numbers less than 10 into 2 digit numbers
 
-    episodesContainer.appendChild(episodeDetails);
-    episodeDetails.appendChild(episodeImage);
-    episodeDetails.appendChild(episodeInfo);
-    episodeInfo.appendChild(episodeName);
-//Asign values to the template
-    episodeImage.src=episode.image.medium;
-    episodeName.innerHTML=episode.name;
- }
+const numberConverter=(num)=>{
+if(num<10){
+    return "0"+num
+}
+else return num;
+}
+
+//A function to enable us to rerender the details with different info
+const createEpisodeTemplate = (episode) => {
+  //Create a section to store each episode detail
+  const episodeDetails = document.createElement("section");
+  episodeDetails.id = "episodeDetails";
+  const episodeImage = document.createElement("img");
+  const episodeInfo = document.createElement("section");
+  //Episode Info
+  const episodeNameAndCode = document.createElement("h4");
+  const episodeSummary = document.createElement("p");
+  //Copyrights
+  const copyrights=document.createElement("p");
+  copyrights.id="copyrights"
+
+  episodesContainer.appendChild(episodeDetails);
+  episodeDetails.appendChild(episodeImage);
+  episodeDetails.appendChild(episodeInfo);
+  episodeInfo.appendChild(episodeNameAndCode);
+  episodeInfo.appendChild(episodeSummary)
+  episodeInfo.appendChild(copyrights);
+
+  //Asign values to the template
+  episodeImage.src = episode.image.medium;
+  episodeNameAndCode.innerHTML = episode.name +" -S"+numberConverter(episode.season)+"E"+numberConverter(episode.number);
+  episodeSummary.innerHTML=episode.summary;
+  copyrights.innerHTML= ' The contents displayed on this episode was extracted from:  '+episode.url
+};
 
 //Loop through the episodes to display them
 
-for(let i=0;i<allEpisodes.length;i++){
-    createEpisodeTemplate(allEpisodes[i]);
+for (let i = 0; i < allEpisodes.length; i++) {
+  createEpisodeTemplate(allEpisodes[i]);
 }
